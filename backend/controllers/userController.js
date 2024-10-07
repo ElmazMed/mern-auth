877a22bb0b3e39547ab9d193f26cdd0d6039994e
ElmazMed <elmazgour.mohamed@gmail.com>
2024-10-07 01:18:50 +0100

    created and connect database, userSchema sat-up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,27 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userSchema.js";
+
+// @desc        Register a new user/Set token
+// route        POST api/users/
+// @access      Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExisits = await User.findOne({ email });
+  const user = await User.create({ name, email, password });
+
+  if (userExisits) {
+    res.status(401);
+    throw new Error("User already registred");
+  } else {
+    res.status(201).json({ id: user._id, name: user.name, email: user.email });
+  }
+});
 
 // @desc        Authenticate user/Set token
 // route        POST api/users/auth
 // @access      Public
 const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ msg: "User login!" });
-});
-
-// @desc        Register a new user/Set token
-// route        POST api/users/
-// @access      Public
-const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ msg: "User registerd!" });
 });
 
 // @desc        User logout/Delete token
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import { router } from "./routes/userRoute.js";
 import { errorHandler, notFound } from "./middleware/errMiddleware.js";
+import { connectDB } from "./database/connectDB.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.json());
 app.use("/api/users", router);
 app.get("/", (req, res) => res.send("Server is ready!"));
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server is listening to port ${port}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is listening to port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
