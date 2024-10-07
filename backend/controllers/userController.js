5248d04cba8b29dddc3f5a25741ff812df640506
ElmazMed <elmazgour.mohamed@gmail.com>
2024-10-07 01:49:26 +0100

    user token is created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userSchema.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc        Register a new user/Set token
 // route        POST api/users/
@@ -7,13 +8,19 @@ import User from "../models/userSchema.js";
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   const userExisits = await User.findOne({ email });
-  const user = await User.create({ name, email, password });
 
   if (userExisits) {
-    res.status(401);
+    res.status(400);
     throw new Error("User already registred");
-  } else {
+  }
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    generateToken(res, user._id);
     res.status(201).json({ id: user._id, name: user.name, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user credentials");
   }
 });
 
